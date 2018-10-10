e1f09630640a3ef17fcbc0b9312dc302cb99b65a
= <kamaljohnson12345@gmail.com>
2018-10-10 19:43:41 +0530

    initialized forms in templates

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,6 +4,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('accounts/', include('django.contrib.auth.urls')),
+    path('', include('django.contrib.auth.urls')),
     path('register/', views.register, name='register'),
 ]
--- restaurant_accounts/models.py
+++ restaurant_accounts/models.py
@@ -1,15 +1,17 @@
 from django.db import models
 from geoposition.fields import GeopositionField
 from django.core.validators import MaxValueValidator, MinValueValidator
+from accounts.models import user_account
 
 
 class Restaurant(models.Model):
+    owner = models.ForeignKey(user_account, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=100)
     rating = models.IntegerField(default=0, validators=[MinValueValidator(0), MaxValueValidator(5)])
     position = GeopositionField()
 
     def __str__(self):
-        return self.name
+        return self.name + " - " + str(self.owner)
 
 
 class ItemType(models.Model):
@@ -26,11 +28,12 @@ class Item(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     price = models.IntegerField()
-    offer_price = models.IntegerField()     # can be used to display the offer price for the product
+    offer_price = models.IntegerField()  # can be used to display the offer price for the product
     rating = models.IntegerField(default=0, validators=[MinValueValidator(0), MaxValueValidator(5)])
 
     def __str__(self):
-        return self.name + ' | ' + str(self.offer_price) + ' Rs | ' + str(self.price - self.offer_price) + ' Rs off |' + ' *' * self.rating
+        return self.name + ' | ' + str(self.offer_price) + ' Rs | ' + str(
+            self.price - self.offer_price) + ' Rs off |' + ' *' * self.rating
 
 
 class OfferType(models.Model):
